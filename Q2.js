e93cec327089ed1b67ad614b2d397f2307d4a904
JAMI-IT <103955140+JAMI-IT@users.noreply.github.com>
2022-05-27 18:30:33 +0500

    TEST

---
+++ Q2.js
@@ -0,0 +1,20 @@
+let a,b;
+
+let op=operator(3,3);
+function operator(a,b)
+{
+    let s,divi,sub,Nult;
+    s=a+b;
+    console.log("SUM a+b ="+s);
+   sub=a-b;
+   console.log("SUB a-b ="+sub);
+    divi=a/b;
+    console.log("DIVISION a/b ="+divi);
+    Mult=a*b;
+    console.log("Multiplication a/b ="+Mult);
+
+}
+
+
+
+
